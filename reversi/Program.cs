7665eec09bc619615ac039d63104f690cf1d6650
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-10-23 15:45:58 +0900

    _(:3」∠)_ コードをきれいに書き直した
    不要な行を削除
    コメントの追加
    readonlyを使って変数を定数にした

--- reversi/Program.cs
+++ reversi/Program.cs
@@ -4,8 +4,10 @@ namespace reversi
 {
     class MainClass
     {
+        // 盤: 横9*縦10 y*9+xと使う。0行目と9行目は番兵
         public static int[] board = new int[91];
-        public static int[] direction = { -10, -9, -8, -1, 1, 8, 9, 10 };
+        // 走査する方向を記憶する配列
+        public static readonly int[] direction = { -10, -9, -8, -1, 1, 8, 9, 10 };
 
         public static void Main(string[] args)
         {
@@ -13,8 +15,11 @@ namespace reversi
             board[40] = board[50] = 1;
             board[41] = board[49] = 2;
 
+
             for (int i = 1; i < 10; i++)
+            {
                 board[i * 9] = 3;
+            }
 
             // ゲーム開始
             play_game();
@@ -30,29 +35,29 @@ namespace reversi
             // ゲームが終了するまでループさせる
             while (true)
             {
-                // 探索するときの基準となるマスを表す
-                int base_point;
-                // 返せる石を保存しておく変数
+                // 返せる石の数を保存しておく変数
                 int n_flippable_disks = 0;
-                // 石を返す必要があるかを制御する変数
-                bool need_flip = false;
 
+                // 盤面を表示
                 show_board();
 
                 // 置けるマスの個数をカウントする
                 n_flippable_disks = count_flippable_disks(player);
 
-                if (n_flippable_disks > 0)
+                if (n_flippable_disks > 0) // 石を置く場所があるとき
                 {
-                    // 石を置く場所がある
-                    if (player == 1)
+                    // 探索するときの基準となるマスを表す
+                    int base_point;
+
+                    if (player == 1) // プレイヤーが人間の場合
                     {
-                        // プレイヤーが人間の場合
+                        // 入力を求める
                         base_point = get_placeable_place(player);
                         flip_disks(base_point, player);
                     }
-                    else
+                    else // プレイヤーがNPCの場合
                     {
+                        // 置ける場所を探索する
                         for (n_flippable_disks = 0, base_point = 8; n_flippable_disks == 0; n_flippable_disks = eight_way_scanning(base_point, player))
                         {
                             base_point++;
@@ -60,14 +65,14 @@ namespace reversi
                         flip_disks(base_point, player);
                     }
                 }
-                else if (!enemy_passed)
+                else if (!enemy_passed) // 石が置けないとき、かつ、相手がパスしていないとき
                 {
-                    // 石が置けないとき
                     enemy_passed = true;
                     Console.WriteLine("passed");
                 }
-                else
+                else // 石が置けず、相手もパスしているとき
                 {
+
                     Console.WriteLine("end");
                     break;
                 }
@@ -87,6 +92,7 @@ namespace reversi
             }
         }
 
+        // 盤の全体から裏返せる石を数える
         public static int count_flippable_disks(int player)
         {
             int n_flippable_disks = 0;
@@ -97,7 +103,7 @@ namespace reversi
             return n_flippable_disks;
         }
 
-        // 8方向を走査して、相手の石の個数を数える
+        // 8方向を走査して、裏返せる石の個数を数える
         public static int eight_way_scanning(int base_point, int player)
         {
             int n_flippable_disks = 0;
@@ -119,42 +125,46 @@ namespace reversi
             return n_flippable_disks;
         }
 
+        // 石を置く場所の入力を求める関数
         public static int get_placeable_place(int player)
         {
             int base_point;
-
-            do
+            while (true)
             {
+                Console.WriteLine("石を置く場所：");
                 string input_string = Console.ReadLine();
                 string[] string_array = input_string.Split();
                 int column = int.Parse(string_array[0]);
                 int raw = int.Parse(string_array[1]);
                 base_point = column + raw * 9;
-            } while (eight_way_scanning(base_point, player) <= 0);
+                if (eight_way_scanning(base_point, player) > 0)
+                {
+                    break;
+                }
+                Console.WriteLine("石を置ける場所を入力してください");
+            }
 
             return base_point;
         }
 
+        // 実際に石を返す
         public static void flip_disks(int base_point, int player)
         {
-            if (board[base_point] == 0)
+            for (int i = 0; i < 8; i++)
             {
-                for (int i = 0; i < 8; i++)
+                int scanning_point, n_enemy_disks = 0;
+                for (scanning_point = base_point + direction[i]; board[scanning_point] == 3 - player; scanning_point += direction[i])
                 {
-                    int scanning_point, n_enemy_disks = 0;
-                    for (scanning_point = base_point + direction[i]; board[scanning_point] == 3 - player; scanning_point += direction[i])
+                    n_enemy_disks++;
+                }
+                if (n_enemy_disks > 0 && board[scanning_point] == player)
+                {
+                    scanning_point = base_point;
+                    do
                     {
-                        n_enemy_disks++;
-                    }
-                    if (n_enemy_disks > 0 && board[scanning_point] == player)
-                    {
-                        scanning_point = base_point;
-                        do
-                        {
-                            board[scanning_point] = player;
-                            scanning_point += direction[i];
-                        } while (board[scanning_point] != player);
-                    }
+                        board[scanning_point] = player;
+                        scanning_point += direction[i];
+                    } while (board[scanning_point] != player);
                 }
             }
         }
