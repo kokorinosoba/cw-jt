30ea502a1da045aaf5680278cc5b692d1cfe3f08
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-11-15 23:15:31 +0900

    :sparkles: BoardとCellクラスを追加

---
+++ reversi/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace reversi
+{
+    public class Board
+    {
+        // 二次元配列のCellを持ったcells
+        List<List<Cell>> cells = new List<List<Cell>>();
+
+        public Board()
+        {
+
+        }
+
+        void show_board()
+        {
+
+        }
+    }
+}
---
+++ reversi/Cell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace reversi
+{
+    public class Cell
+    {
+        public int disk;
+
+        public Cell()
+        {
+
+        }
+
+        public char diskMark()
+        {
+            return ' ';
+        }
+
+        public void putDisk(int diskColor)
+        {
+
+        }
+
+        public void flipDisk()
+        {
+
+        }
+    }
+}
